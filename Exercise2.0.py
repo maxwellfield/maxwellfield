ace20d5310e6fda929c4c3940d2cceeda40f9a8d
maxwellfield <90723569+maxwellfield@users.noreply.github.com>
2021-09-14 16:29:55 -0400

    Add files via upload

---
+++ Exercise2.0.py
@@ -0,0 +1,4 @@
+print('Maxwell Field')
+print('September 14, 2021')
+print('At what size of a project should you start using version management software? Why?')
+print('We recommend repositories remain small, ideally less than 1 GB, and less than 5 GB is strongly recommended. Smaller repositories are faster to clone and easier to work with and maintain.')
