403b43e378cc67447dabbe60c5973870c4a43a56
maxwellfield <90723569+maxwellfield@users.noreply.github.com>
2021-09-14 16:29:40 -0400

    Delete Exercise2.0.py

--- Exercise2.0.py
+++
@@ -1,4 +0,0 @@
-print('Maxwell Field')
-print('September 14, 2021')
-print('At what size of a project should you start using version management software? Why?')
-print('1 GB because smaller repositories are faster to clone and easier to work with and maintain.')
