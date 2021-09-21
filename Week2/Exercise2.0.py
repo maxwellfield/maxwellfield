2c55ce5e9943f862ffa1755aef75203269075cf6
maxwellfield <90723569+maxwellfield@users.noreply.github.com>
2021-09-21 15:58:28 -0400

    Add files via upload

---
+++ Week2/Exercise2.0.py
@@ -0,0 +1,4 @@
+print('Maxwell Field')
+print('September 19, 2021')
+print('At what size of a project should you start using version management software? Why?')
+print('It is subjective based on the needs of the user. Certain users may find that any size project benefits from using version management software. Other users might not benefit until they have reached a barrier. Having an online community available to check your code and provide feedback is an amazing resource. In terms of storage, GitHub recommends that repositories remain small, ideally less than 1 GB, and less than 5 GB is strongly recommended. Smaller repositories are faster to clone and easier to work with and maintain.')
