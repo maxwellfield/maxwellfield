edd352bdf6a17ce37221c1bf3731a7b0310b881b
maxwellfield <90723569+maxwellfield@users.noreply.github.com>
2021-10-12 12:41:51 -0400

    Update ex_05_02.py

--- CIS104A/Module5/ex_05_02.py
+++ CIS104A/Module5/ex_05_02.py
@@ -5,15 +5,15 @@ while True:
     if num == "done":
         break
     try:
-        fval = float(num)
+        val = float(num)
     except:
         print('Invalid input')
         continue
 
-    if largest is None or fval > largest:
-        largest = fval
-    if smallest is None or fval < smallest:
-        smallest = fval
+    if largest is None or val > largest:
+        largest = val
+    if smallest is None or val < smallest:
+        smallest = val
 
 print("Maximum is", int(largest))
 print("Minimum is", int(smallest))
